9f7327bbf5d48554ae94b0ed30c91e19e94fe613
Atharva Hinge <atharva.3936@gmail.com>
2022-11-27 20:00:56 +0530

    Update ifelse - 11.js

--- ifelse - 11.js
+++ ifelse - 11.js
@@ -38,3 +38,60 @@ else if(time >=16 && time<=20){
 else{
     console.log("Good Night")
 }
+
+// This was all about the basics of if else statements, now let's study another concept known as switch statements.
+
+/* What switch statements do?
+
+-> We can specify multiple cases, just like if else, but in a less complicated manner.
+-> Performing operations based on a default case becomes easier with the use of switch statements.
+
+*/
+
+
+// We print the name of the month on the basis of a case:
+
+let a = prompt("What is the number? ");
+let a1 = parseInt(a);
+
+switch (a1) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+  case 5:
+    console.log("May");
+    break;
+  case 6:
+    console.log("June");
+    break;
+  case 7:
+    console.log("July");
+    break;
+  case 8:
+    console.log("August");
+    break;
+  case 9:
+    console.log("September");
+    break;
+  case 10:
+    console.log("October");
+    break;
+  case 11:
+    console.log("November");
+    break;
+  case 12:
+    console.log("December");
+    break;
+  default:
+    console.log("Specify a number between 1 and 12 only.");
+    break;
+}
