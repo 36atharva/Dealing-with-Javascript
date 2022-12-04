3b59f362418184805f31061c3c202967fd39411b
Atharva Hinge <atharva.3936@gmail.com>
2022-12-04 19:53:21 +0530

    Update loops-12.js

--- loops-12.js
+++ loops-12.js
@@ -6,3 +6,20 @@ for(let i=1; i<=10; i++)
 {
     console.log(i + " "); // logs 1 to 10.
 }
+
+/* Why do we need Loops?
+
+-> Wanting to run a sequence of code over and over can become lengthy.
+-> We can specify certain conditions and run a loop to print, or execute a statement.
+-> Loops are very important as a concept, even in DSA loops are used often.
+
+*/
+
+// 1. for loop:
+
+for(let i =1; i<= 10; i++)
+{
+    console.log(i); // logs number 1 to 10.
+}
+
+// 2. 
