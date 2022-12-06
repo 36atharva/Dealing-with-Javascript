9cb9f2f9ba8edd4c090115540b55f917497dcca8
Atharva Hinge <atharva.3936@gmail.com>
2022-12-06 20:08:18 +0530

    added type conversion

---
+++ type-conversion 13.js
@@ -0,0 +1,25 @@
+/*
+Type conversion is an important functionality which javascript allows us to do!
+
+-> integer to string
+-> string to integer etc.
+
+There are 4 primitive data types in Javascript namely:
+~ string
+~ number
+~ boolean
+~ object
+
+*/
+
+// Checking the type of data:
+
+let a = 12;
+let b = "atharva";
+let c = "a";
+let arr = [1, 2, 3, 4];
+
+console.log(typeof a); // logs number
+console.log(typeof b); // logs string
+console.log(typeof c); // logs string even when only 1 character!
+console.log(typeof arr); // logs object (there is no such type as array, array is a type of object itself)
