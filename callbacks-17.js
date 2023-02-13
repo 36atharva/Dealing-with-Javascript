2eebd3ca1021314968b2f02f2370869349ef157b
Atharva Hinge <89994686+36atharva@users.noreply.github.com>
2023-02-13 21:20:21 +0530

    Create callbacks-17.js

---
+++ callbacks-17.js
@@ -0,0 +1,12 @@
+/* We are nearing the end of our Javascript Series, in this snippet, let's take a look at Callback functions in Javascript and why are they used.
+
+What are Callback functions?
+- A Callback function is simply a function passed to another function as an argument.
+
+Let's define 2 functions and see which one logs to the console:
+
+*/
+
+function first(){
+   
+}
