0660bfd9dcbb557ab6b829c8d5b5598719803ef5
Atharva Hinge <89994686+36atharva@users.noreply.github.com>
2023-03-06 20:25:13 +0530

    Update callbacks-18.js

--- callbacks-18.js
+++ callbacks-18.js
@@ -28,3 +28,22 @@ second();
 display();
 
 // This logs 5 and then 10, what we understand from this is that javascript functions are executed by the sequence we call them, here it does not matter if we define the second function before first or vice versa
+
+/*
+
+*** IMPORTANT NOTE ***
+When you call a function in some another function's parameters, do not use the parenthesis
+Example:
+
+myFunction(){
+  some work
+}
+
+myOtherFunction(a,b,myFunction) Correct way
+myOtherFunction(a,b,myFunction()) Wrong way
+
+We mostly use Callback functions in Asynchronous functionalities, like async/await.
+
+These topics will be covered in Advanced Javascript Section 0.0
+
+*/
